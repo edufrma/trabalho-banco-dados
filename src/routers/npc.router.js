f03a4ceecbadcbbad30584e7d06002547c2f8496
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-06 18:04:39 -0300

    Update npc.router.js

--- src/routers/npc.router.js
+++ src/routers/npc.router.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createNPC, getAllNPCs } from '../controllers/npc.controller.js';
+import { createNPC, getAllNPCs, getAllEnemies } from '../controllers/npc.controller.js';
 
 const npcRouter = express.Router();
 
 npcRouter.post('/npcs', createNPC);
 npcRouter.get('/npcs', getAllNPCs);
+npcRouter.get('/inimigos', getAllEnemies);
 
 export default npcRouter;
