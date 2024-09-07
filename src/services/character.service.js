91cfc4f9b3b3889bd7631a8cbc6fa20adf1a929f
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-06 23:28:51 -0300

    Update character.service.js

--- src/services/character.service.js
+++ src/services/character.service.js
@@ -1,22 +1,21 @@
 import * as personagemRepository from '../repository/character.repository.js';
 
-export async function createPersonagem(nome, nivel, nome_classe, controladorId) {
-  return await personagemRepository.createPersonagem(nome, nivel, nome_classe, controladorId);
+export async function createPersonagem(nome, nivel, nome_classe, controladorId, foto) {
+  return await personagemRepository.createPersonagem(nome, nivel, nome_classe, controladorId, foto);
 }
 
 export async function getPersonagensByControlador(controladorId) {
     return await personagemRepository.getPersonagensByControlador(controladorId);
 }
-  
+
 export async function getPersonagemById(id) {
     return await personagemRepository.getPersonagemById(id);
 }
-  
-export async function updatePersonagem(id, nivel, nome_classe) {
-    return await personagemRepository.updatePersonagem(id, nivel, nome_classe);
+
+export async function updatePersonagem(id, nivel, nome_classe, foto) {
+    return await personagemRepository.updatePersonagem(id, nivel, nome_classe, foto);
 }
-  
+
 export async function deletePersonagem(id) {
     return await personagemRepository.deletePersonagem(id);
 }
-  
