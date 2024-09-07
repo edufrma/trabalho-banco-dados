6c85848ab5244ca16159ec674ddb78c1d693a29a
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-06 23:35:27 -0300

    Update character.router.js

--- src/routers/character.router.js
+++ src/routers/character.router.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { createPersonagem, getPersonagensByControlador, updatePersonagem, deletePersonagem } from '../controllers/character.controller.js';
 import { tokenValidation } from '../middlewares/token.validation.js';
-const personagemRouter = express.Router();
+import multer from 'multer';
 
-personagemRouter.post('/personagem', tokenValidation, createPersonagem); 
-personagemRouter.get('/personagens', tokenValidation, getPersonagensByControlador); 
-personagemRouter.put('/personagem/:id', tokenValidation, updatePersonagem); 
-personagemRouter.delete('/personagem/:id', tokenValidation, deletePersonagem); 
+const personagemRouter = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
+personagemRouter.post('/personagem', tokenValidation, upload.single('foto'), createPersonagem);
+personagemRouter.get('/personagens', tokenValidation, getPersonagensByControlador);
+personagemRouter.put('/personagem/:id', tokenValidation, upload.single('foto'), updatePersonagem);
+personagemRouter.delete('/personagem/:id', tokenValidation, deletePersonagem);
 
 export default personagemRouter;
