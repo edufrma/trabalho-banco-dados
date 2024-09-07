e22ec627bb2f79e8c7885f42b028ccc1d74c83f0
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-06 23:26:05 -0300

    Update npc.repository.js

--- src/repository/npc.repository.js
+++ src/repository/npc.repository.js
@@ -2,7 +2,7 @@ import { db } from "../database.js";
 
 export async function getAllNPCsWithMissions() {
   const query = `
-    SELECT n.id AS npc_id, n.nome AS npc_nome, n.tipo, m.nome AS missao_nome
+    SELECT n.id AS npc_id, n.nome AS npc_nome, n.tipo, encode(n.foto, 'base64') AS foto, m.nome AS missao_nome
     FROM npc n
     LEFT JOIN missoes m ON n.id = m.id_fornecedor;
   `;
@@ -10,20 +10,20 @@ export async function getAllNPCsWithMissions() {
   return result.rows;
 }
 
-export async function createNPC(nome, tipo) {
+export async function createNPC(nome, tipo, foto) {
     const query = `
-      INSERT INTO npc (nome, tipo)
-      VALUES ($1, $2)
-      RETURNING id, nome, tipo;
+      INSERT INTO npc (nome, tipo, foto)
+      VALUES ($1, $2, $3)
+      RETURNING id, nome, tipo, encode(foto, 'base64') AS foto;
     `;
-    const values = [nome, tipo];
+    const values = [nome, tipo, foto];
     const result = await db.query(query, values);
     return result.rows[0];
 }
 
 export async function getAllEnemiesWithSkills() {
     const query = `
-      SELECT i.id AS inimigo_id, i.nome AS inimigo_nome, i.nivel, h.nome AS habilidade_nome, h.dano, h.custo
+      SELECT i.id AS inimigo_id, i.nome AS inimigo_nome, i.nivel, encode(i.foto, 'base64') AS foto, h.nome AS habilidade_nome, h.dano, h.custo
       FROM inimigo i
       LEFT JOIN inimigo_habilidades ih ON i.id = ih.id_inimigo
       LEFT JOIN habilidades h ON ih.nome_habilidade = h.nome;
@@ -31,4 +31,3 @@ export async function getAllEnemiesWithSkills() {
     const result = await db.query(query);
     return result.rows;
 }
-  
