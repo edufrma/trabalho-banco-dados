b13f408474cb76d97882142a75fc5b623dc05b01
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-06 23:34:57 -0300

    Update npc.router.js

--- src/routers/npc.router.js
+++ src/routers/npc.router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { createNPC, getAllNPCs, getAllEnemies } from '../controllers/npc.controller.js';
+import multer from 'multer';
 
 const npcRouter = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
-npcRouter.post('/npcs', createNPC);
+npcRouter.post('/npcs', upload.single('foto'), createNPC);
 npcRouter.get('/npcs', getAllNPCs);
 npcRouter.get('/inimigos', getAllEnemies);
 
