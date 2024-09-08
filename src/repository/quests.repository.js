c08f8a3f2fb7f14af9e7518654fe3039d9d33298
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-08 13:19:44 -0300

    Update quests.repository.js

--- src/repository/quests.repository.js
+++ src/repository/quests.repository.js
@@ -10,3 +10,24 @@ export async function getMissionsWithRewards() {
   const result = await db.query(query);
   return result.rows;
 }
+
+export async function getMissoesRecompensasByJogador(controladorId) {
+  const query = `
+    SELECT 
+      m.Nome AS Missao, 
+      m.Objetivo, 
+      i.Nome AS Item, 
+      r.Quantidade,
+      p.Nome AS Personagem
+    FROM Missoes m
+    JOIN Recompensa r ON m.Nome = r.Nome_missao
+    JOIN Item i ON r.Nome_item = i.Nome
+    JOIN Participa_missoes pm ON m.Nome = pm.Nome_missao
+    JOIN Personagem p ON pm.id_personagem = p.id
+    WHERE p.controlador = $1;
+  `;
+  const values = [controladorId];
+  
+  const result = await db.query(query, values);
+  return result.rows;
+}
