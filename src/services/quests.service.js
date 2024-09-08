93b91bdb20fa4e799bf61068e8cdd6ef6d8a6a4e
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-08 13:20:22 -0300

    Update quests.service.js

--- src/services/quests.service.js
+++ src/services/quests.service.js
@@ -3,3 +3,12 @@ import * as questsRepository from "../repository/quests.repository.js";
 export async function fetchMissionsWithRewards() {
   return await questsRepository.getMissionsWithRewards();
 }
+
+export async function fetchMissoesRecompensasByJogador(controladorId) {
+  try {
+    const missoesRecompensas = await questsRepository.getMissoesRecompensasByJogador(controladorId);
+    return missoesRecompensas;
+  } catch (error) {
+    throw new Error(`Erro ao buscar missões e recompensas: ${error.message}`);
+  }
+}
