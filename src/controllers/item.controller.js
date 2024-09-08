cf480d7cba82c67685315caadd42ec20ccfc5f3d
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-08 12:04:56 -0300

    Update item.controller.js

--- src/controllers/item.controller.js
+++ src/controllers/item.controller.js
@@ -1,10 +1,17 @@
 import * as itemService from '../services/item.service.js';
 
 export async function getPersonagemItens(req, res) {
-  try {
-    const itens = await itemService.fetchPersonagemItens();
-    res.status(200).json(itens);
-  } catch (error) {
-    res.status(500).send('Erro ao buscar itens dos personagens');
-  }
+    const controladorId = res.locals.user.id;
+    
+    try {
+      const itens = await itemService.fetchPersonagemItensByControlador(controladorId);
+      
+      if (itens.length === 0) {
+        return res.status(404).send('Nenhum item encontrado para os personagens deste jogador.');
+      }
+  
+      res.status(200).json(itens);
+    } catch (error) {
+      res.status(500).send('Erro ao buscar itens dos personagens');
+    }
 }
