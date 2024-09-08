c047a6e2102a6147231faf8fb34ed7cc12079608
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-08 13:22:54 -0300

    Update quests.controller.js

--- src/controllers/quests.controller.js
+++ src/controllers/quests.controller.js
@@ -8,3 +8,18 @@ export async function getMissions(req, res) {
     res.status(500).send('Error fetching missions with rewards');
   }
 }
+
+
+export async function getMissoesRecompensas(req, res) {
+  const controladorId = res.locals.user.id; 
+  try {
+    const missoesRecompensas = await questsService.fetchMissoesRecompensasByJogador(controladorId);
+    if (!missoesRecompensas || missoesRecompensas.length === 0) {
+      return res.status(404).json({ message: 'Nenhuma missão encontrada para esse jogador' });
+    }
+
+    res.status(200).json(missoesRecompensas);
+  } catch (error) {
+    res.status(500).json({ message: `Erro ao buscar missões: ${error.message}` });
+  }
+}
--- src/routers/quests.router.js
+++ src/routers/quests.router.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getMissions } from '../controllers/quests.controller.js';
+import { getMissions, getMissoesRecompensas } from '../controllers/quests.controller.js';
+import { tokenValidation } from '../middlewares/token.validation.js';
 
 const questsRouter = express.Router();
 
 questsRouter.get('/quests', getMissions);
+questsRouter.get('/questsandrewards', tokenValidation, getMissoesRecompensas);
+
 
 export default questsRouter;
