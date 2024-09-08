d123a194ef4738189f0f10c8e3b32cd5616be74b
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-08 12:02:25 -0300

    Update item.repository.js

--- src/repository/item.repository.js
+++ src/repository/item.repository.js
@@ -1,13 +1,15 @@
 import { db } from '../database.js';
 
-export async function getPersonagemItens() {
-  const query = `
-    SELECT p.Nome AS personagem, i.Nome AS item, pi.Quantidade
-    FROM Personagem p
-    JOIN Personagens_itens pi ON p.id = pi.id_personagem
-    JOIN Item i ON pi.Nome_item = i.Nome;
-  `;
-
-  const result = await db.query(query);
-  return result.rows;
+export async function getPersonagemItensByControlador(controladorId) {
+    const query = `
+      SELECT p.Nome AS personagem, i.Nome AS item, pi.Quantidade
+      FROM Personagem p
+      JOIN Personagens_itens pi ON p.id = pi.id_personagem
+      JOIN Item i ON pi.Nome_item = i.Nome
+      WHERE p.controlador = $1;
+    `;
+  
+    const values = [controladorId];
+    const result = await db.query(query, values);
+    return result.rows;
 }
